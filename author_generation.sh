03eb3fc070847b72f7a68603d84af119e0b8fa85
TitusCharlie <tituslifecharlie@gmail.com>
2023-05-20 16:50:21 +0100

    refactor auto_generaiton

--- author_generation.sh
+++ author_generation.sh
@@ -5,6 +5,9 @@ set -e
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOTDIR="$(git -C "$SCRIPTDIR" rev-parse --show-toplevel)"
 
+echo "ROOTDIR = $ROOTDIR"
+echo "SCRIPTDIR = $SCRIPTDIR"
+
 set -x
 
 cat > "${ROOTDIR}/AUTHORS" <<- EOF
